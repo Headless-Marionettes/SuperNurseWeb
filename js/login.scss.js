c5a94b5cad5a8d455ef743cc3ea6983a12eeb400
Ignat Pechkurenko <pechkig@gmail.com>
2019-11-24 16:41:54 -0500

    Added first version of patient list

--- js/login.scss.js
+++ js/login.scss.js
@@ -1,24 +1,24 @@
-$("#sign-in-form").each(function () {
+$("#sign-in-form").each(function() {
     var form = this;
     var ajax_form = $(this);
 
     // Suppress the default bubbles
-    form.addEventListener("invalid", function (event) {
+    form.addEventListener("invalid", function(event) {
         event.preventDefault();
     }, true);
 
-    $("button[type=submit]", form).on("click", function (event) {
+    $("button[type=submit]", form).on("click", function(event) {
 
         //Destroy error message from previous runs
-        $("input", form).each(function () {
+        $("input", form).each(function() {
             var field = $(this).css('border-color', '#ced4da')
                 .parent().next("span.form-error-message").text("no error")
                 .addClass("hidden");;
         });
 
         //loop through all invalid fields
-        var invalidFields = $(":invalid", form).each(function () {
-            if($(this)[0].validationMessage.length > 2) {
+        var invalidFields = $(":invalid", form).each(function() {
+            if ($(this)[0].validationMessage.length > 2) {
                 $(this).parent().next("span.form-error-message")
                     .html('&#9785; ' + $(this)[0].validationMessage)
                     .removeClass("hidden");
@@ -26,22 +26,22 @@ $("#sign-in-form").each(function () {
             $(this).css('border-color', '#e53935')
         });
 
-        if (invalidFields.length <= 0){
+        if (invalidFields.length <= 0) {
             event.preventDefault();
 
             var url = "https://super-nurse.herokuapp.com/auth/signin"; //form.attr('action');
 
             $.ajax({
-                type   : "POST",
-                url    : url,
-                data   : ajax_form.serialize(), // serializes the form's elements.
-                success: function (data) {
+                type: "POST",
+                url: url,
+                data: ajax_form.serialize(), // serializes the form's elements.
+                success: function(data) {
                     let token = data.token;
                     window.localStorage.setItem("token", token);
                     window.location.href = "./patients-list.html";
                     // console.log(token);
                 }
-            }).fail(function (data) {
+            }).fail(function(data) {
                 console.log(data);
             });
 
---
+++ js/patients-list.js
@@ -0,0 +1,44 @@
+var load = function() {
+    $.ajaxSetup({
+        beforeSend: function(request) {
+            console.log("before send")
+            var token = "JWT " + window.localStorage.getItem("token");
+            console.log(token)
+            request.setRequestHeader("Authorization", token);
+        }
+    });
+
+    $.ajax({
+        url: "https://super-nurse.herokuapp.com/patients",
+        action: "GET",
+        context: document.body,
+        crossDomain: true,
+        success: function(data) {
+            for (var i = 0; i < data.length; i++) {
+                $('#patients').append(
+                    `<div class="card-wrapper">
+                    <a class="card-link row" href="#">
+                        <div class="image-wraper col-6 col-sm-12">
+                            <img src="./images/senior-placeholder.jpg" alt="" class="img-fluid">
+                        </div>
+                        <div class="content col-6 col-sm-12">
+                            <p class="name subheading">${data[i].first_name + " " + data[i].last_name}</p>
+                            <div class="details">
+                                <div class="details-row">
+                                    <p class="details-label">Date of birth: </p>
+                                    <p class="details-value">${data[i].date_of_birth}</p>
+                                </div>
+                                <div class="details-row">
+                                    <p class="details-label">Room: </p>
+                                    <p class="details-value">${data[i].room}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>`)
+            }
+        }
+    }).fail(function() {
+        $("#list_of_items").html("error");
+    })
+}
