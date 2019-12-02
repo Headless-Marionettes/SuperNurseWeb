4825fb96d3af7a52ba8698f3b70527cb6852d0b5
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2019-12-02 15:39:48 -0500

    added ajax for getting reports

--- js/patient-profile.js
+++ js/patient-profile.js
@@ -34,4 +34,37 @@ var load = function() {
             $("#patient-allergies").html("error");
         }
     })
+
+    $.ajax({
+        url: "https://super-nurse.herokuapp.com/patients/" + id + "/records",
+        action: "GET",
+        context: document.body,
+        crossDomain: true,
+        success: function(data) {
+            if (data.length == 0) {
+                $('#listReports').append(`<h2>There are no reports</h2>`)
+            }
+            for (var i = 0; i < data.length; i++) {
+                $('#listReports').append(
+                `<div class="shadow border rounded p-3 mb-3">
+                    <h3 class="text-dark-primary">Report from ${data[i].date}</h3>
+                    <div class="row">
+                        <div class="col-md font-weight-bold mt-4">Blood Pressure: ${data[i].blood_pressure}</div>
+                        <div class="col-md font-weight-bold mt-4">Respiratory: ${data[i].respiratory_rate}</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md font-weight-bold mt-4">Blood Oxygen: ${data[i].blood_oxygen_level}</div>
+                        <div class="col-md font-weight-bold mt-4">Heart Beat Rate: ${data[i].heart_beat_rate}</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md font-weight-bold mt-4">Additional Information: </div>
+                    </div>
+                </div>`
+                )
+            }
+        },
+        error: function() {
+            $('#listReports').html('Error')
+        }
+    })
 }
